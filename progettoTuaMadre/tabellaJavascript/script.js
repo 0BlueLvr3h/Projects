c074a01ac59190e8fcc1036ab87713b3a504154f
BuildTools <unconfigured@null.spigotmc.org>
2023-04-12 10:10:36 +0200

    xiao

--- progettoTuaMadre/tabellaJavascript/script.js
+++ progettoTuaMadre/tabellaJavascript/script.js
@@ -4,12 +4,15 @@ document.addEventListener("DOMContentLoaded",()=>{
     document.getElementById("submit").addEventListener("click",()=>{
         var cols = document.getElementById("cols").value;
         var rows = document.getElementById("rows").value;
-        var content = document.getElementById("content").value;
-        createTable(cols,rows,content);
+
+        if(cols > 0 && rows >0){
+            createTable(cols,rows);
+        }
+
     })
 
 
-    function createTable(cols,rows,content){
+    function createTable(cols,rows){
         var newTable = document.getElementById("newTable");
 
         var table = document.createElement("table");
@@ -20,22 +23,29 @@ document.addEventListener("DOMContentLoaded",()=>{
                 var tr = document.createElement("tr");
     
                 for(var j = 0; j < cols; j++){
-                    var td = document.createElement("td");
-                    var node = document.createTextNode(content);
-    
-                    td.appendChild(node);
-                    tr.appendChild(td);
+                    if(i==0){
+                        var th = document.createElement("th");
+                        var node = document.createTextNode("Header" + " " + (j + 1));
+                        
+                        th.appendChild(node);
+                        tr.appendChild(th);
+
+                    }else{
+                        var td = document.createElement("td");
+                        var node = document.createTextNode("Cella" + " " + (j + 1));
+        
+                        td.appendChild(node);
+                        tr.appendChild(td);
+                    }
                 }
                 table.appendChild(tr);
             }
     
             newTable.appendChild(table);
-            table.setAttribute("border","1");
-            
-            console.log(newTable);
+            //table.setAttribute("border","1");
+            table.style.margin = "auto";
+            table.style.textAlign = "center";
+            table.style.border = "1px solid black"
         }
-        
-
     }
-
 })
